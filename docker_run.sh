f2e2318227cf90730149c2694a468340b5bcbf57
gomesuit <gomesuit@gmail.com>
2016-03-11 22:48:21 +0900

    jst

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --name mysql -v $PWD:/etc/mysql/conf.d --volumes-from mysqlvolume -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 127.0.0.1:3306:3306 -d mysql:5.6
+docker run --name mysql -v $PWD:/etc/mysql/conf.d --volumes-from mysqlvolume -e TZ=Japan -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 127.0.0.1:3306:3306 -d mysql:5.6
